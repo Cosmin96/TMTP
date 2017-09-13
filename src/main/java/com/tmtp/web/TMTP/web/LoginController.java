7fa57c545fd4057a1e2d41b07fa00b3ac68a7aa0
Cosmin Nicolae Bresug <Cosmin.NicolaeBresug@Rightmove.co.uk>
2017-09-13 13:42:04 +0100

    Added logout functionality

--- src/main/java/com/tmtp/web/TMTP/web/LoginController.java
+++ src/main/java/com/tmtp/web/TMTP/web/LoginController.java
@@ -5,6 +5,9 @@ import com.tmtp.web.TMTP.entity.VideoPosts;
 import com.tmtp.web.TMTP.security.SecurityService;
 import com.tmtp.web.TMTP.security.UserService;
 import com.tmtp.web.TMTP.security.UserValidator;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -12,6 +15,8 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.util.Collections;
 import java.util.List;
 
@@ -83,4 +88,12 @@ public class LoginController {
         return "index";
     }
 
+    @RequestMapping(value="/logout", method = RequestMethod.GET)
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null){
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+        }
+        return "redirect:/register";
+    }
 }
