49b6d5121c0ad5369357cb89d9941eced6f88c4f
Cosmin Bresug <marnicos2006@yahoo.com>
2017-10-13 12:40:16 +0100

    Abusive comments filtering system with automated ban

--- src/main/java/com/tmtp/web/TMTP/web/PostController.java
+++ src/main/java/com/tmtp/web/TMTP/web/PostController.java
@@ -3,27 +3,46 @@ package com.tmtp.web.TMTP.web;
 import com.tmtp.web.TMTP.entity.Comment;
 import com.tmtp.web.TMTP.entity.User;
 import com.tmtp.web.TMTP.entity.VideoPosts;
+import com.tmtp.web.TMTP.security.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class PostController {
 
     private final UserDataFacade userDataFacade;
+    private final UserService userService;
     private final VideoPostsFacade videoPostsFacade;
 
     public PostController(final UserDataFacade userDataFacade,
+                          final UserService userService,
                           final VideoPostsFacade videoPostsFacade) {
         this.userDataFacade = userDataFacade;
+        this.userService = userService;
         this.videoPostsFacade = videoPostsFacade;
     }
 
     @RequestMapping(value = "/newPost", method = RequestMethod.POST)
-    public String createNewPost(@ModelAttribute("videoPostForm") VideoPosts videoPosts, Model model){
+    public String createNewPost(@ModelAttribute("videoPostForm") VideoPosts videoPosts, Model model, RedirectAttributes redirectAttributes){
+
+        if(videoPostsFacade.filterComment(videoPosts.getDescription())){
+            User user = userDataFacade.retrieveLoggedUser();
+            user.getPoints().setYellow(user.getPoints().getYellow() + 1);
+            if(user.getPoints().getYellow() == 2){
+                user.getPoints().setYellow(0);
+                user.getPoints().setRed(user.getPoints().getRed() + 1);
+                user.setBanned(true);
+            }
+            userService.updateUser(user);
+            redirectAttributes.addFlashAttribute("error", true);
+            redirectAttributes.addFlashAttribute("errorMessage", "You have used abusive language and we have penalized you as appropriate");
+            return "redirect:/home";
+        }
         videoPostsFacade.createVideoPost(videoPosts, userDataFacade.retrieveLoggedUser());
         return "redirect:/home";
     }
@@ -50,9 +69,21 @@ public class PostController {
 
     @RequestMapping(value = "/newComment/{id}", method = RequestMethod.POST)
     public String postComment(@ModelAttribute("commentForm") Comment comment,
-                              @PathVariable("id") String id, Model model){
-
+                              @PathVariable("id") String id, Model model, RedirectAttributes redirectAttributes){
         VideoPosts videoPosts = videoPostsFacade.retrievePostById(id);
+        if(videoPostsFacade.filterComment(comment.getComment())){
+            User user = userDataFacade.retrieveLoggedUser();
+            user.getPoints().setYellow(user.getPoints().getYellow() + 1);
+            if(user.getPoints().getYellow() == 2){
+                user.getPoints().setYellow(0);
+                user.getPoints().setRed(user.getPoints().getRed() + 1);
+                user.setBanned(true);
+            }
+            userService.updateUser(user);
+            redirectAttributes.addFlashAttribute("error", true);
+            redirectAttributes.addFlashAttribute("errorMessage", "You have used abusive language and we have penalized you as appropriate");
+            return "redirect:/post/" + id;
+        }
         videoPostsFacade.addNewComment(videoPosts, comment, userDataFacade.retrieveLoggedUser());
 
         return "redirect:/post/" + id;
--- src/main/java/com/tmtp/web/TMTP/web/VideoPostsFacade.java
+++ src/main/java/com/tmtp/web/TMTP/web/VideoPostsFacade.java
@@ -3,6 +3,7 @@ package com.tmtp.web.TMTP.web;
 import com.tmtp.web.TMTP.entity.Comment;
 import com.tmtp.web.TMTP.entity.User;
 import com.tmtp.web.TMTP.entity.VideoPosts;
+import com.tmtp.web.TMTP.service.CommentFilteringService;
 import com.tmtp.web.TMTP.service.VideoPostsService;
 import org.springframework.stereotype.Component;
 
@@ -12,9 +13,12 @@ import java.util.List;
 public class VideoPostsFacade {
 
     private final VideoPostsService videoPostsService;
+    private final CommentFilteringService commentFilteringService;
 
-    public VideoPostsFacade(final VideoPostsService videoPostsService) {
+    public VideoPostsFacade(final VideoPostsService videoPostsService,
+                            final CommentFilteringService commentFilteringService) {
         this.videoPostsService = videoPostsService;
+        this.commentFilteringService = commentFilteringService;
     }
 
     public List<VideoPosts> retrieveListOfVideoPosts(){
@@ -35,6 +39,10 @@ public class VideoPostsFacade {
 
     public void addNewComment(VideoPosts videoPosts, Comment comment, User user){
         videoPostsService.addNewComment(videoPosts, comment, user);
+    }
+
+    public boolean filterComment(String comment){
+        return commentFilteringService.filterComment(comment);
     }
 
 }
