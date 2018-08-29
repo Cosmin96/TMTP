1721581baa391a4e5a3120daba45a07c8bda8d6f
blr-Oliver <vasily.liaskovsky@gmail.com>
2018-08-29 19:11:59 +0300

    chat completely removed only after long delay

--- src/main/java/com/tmtp/web/TMTP/repository/ChatMessageRepository.java
+++ src/main/java/com/tmtp/web/TMTP/repository/ChatMessageRepository.java
@@ -1,18 +1,20 @@
 package com.tmtp.web.TMTP.repository;
 
-import com.tmtp.web.TMTP.dto.enums.MessageType;
-import com.tmtp.web.TMTP.entity.ChatMessage;
+import java.util.List;
+
 import org.joda.time.DateTime;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import com.tmtp.web.TMTP.dto.enums.MessageType;
+import com.tmtp.web.TMTP.entity.ChatMessage;
 
 @Repository
 public interface ChatMessageRepository extends MongoRepository<ChatMessage, String> {
 
     List<ChatMessage> findByName(String name);
 
-    List<ChatMessage> findByMessageTypeAndDateTimeBefore(
-            MessageType messageType, DateTime dateTime);
+    List<ChatMessage> findByMessageTypeAndDateTimeBefore(MessageType messageType, DateTime dateTime);
+
+    List<ChatMessage> findByDateTimeBefore(DateTime dateTime);
 }
--- src/main/java/com/tmtp/web/TMTP/schedulers/ChatDeletionCronJob.java
+++ src/main/java/com/tmtp/web/TMTP/schedulers/ChatDeletionCronJob.java
@@ -1,20 +1,38 @@
 package com.tmtp.web.TMTP.schedulers;
 
-import com.tmtp.web.TMTP.repository.ChatMessageRepository;
+import java.util.List;
+
+import org.joda.time.DateTime;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import com.tmtp.web.TMTP.entity.ChatMessage;
+import com.tmtp.web.TMTP.repository.ChatMessageRepository;
+import com.tmtp.web.TMTP.web.ChatController;
+
 @Component
 public class ChatDeletionCronJob {
+    private static final Logger LOG = LoggerFactory.getLogger(ChatController.class);
 
+    private static final long TASK_PERIOD = 1000 * 60 * 60 * 24; // milliseconds, 24 hours
+    private static final long TASK_DELAY = 0;  // milliseconds
+    private static final int MESSAGE_LIVE_TIME = 60 * 60 * 24 * 10; // seconds, 10 days
+    
     private ChatMessageRepository chatMessageRepository;
 
     public ChatDeletionCronJob(ChatMessageRepository chatMessageRepository) {
         this.chatMessageRepository = chatMessageRepository;
     }
 
-    @Scheduled(fixedRate = 86400000)
-    public void reportCurrentTime() {
-        chatMessageRepository.deleteAll();
+    @Scheduled(fixedRate = TASK_PERIOD, initialDelay = TASK_DELAY)
+    public void run() {
+        // TODO make sure no audio messages removed from database
+        // before corresponding audio file is removed from cloud storage
+        DateTime cutDownDate = DateTime.now().minusSeconds(MESSAGE_LIVE_TIME);
+        List<ChatMessage> oldMessages = chatMessageRepository.findByDateTimeBefore(cutDownDate);
+        LOG.info("Deleting chat messages dated before {}: found {} message(s)", cutDownDate, oldMessages.size());
+        chatMessageRepository.delete(oldMessages);
     }
 }
